0d4f19688aba80f4eabb9491a0bfa2db792e59fb
artium_96 <artium20@gmail.com>
2022-08-07 00:58:37 +0300

    change

--- jsFile/dogs.js
+++ jsFile/dogs.js
@@ -1,57 +1,68 @@
-//////////////////////////////////////////תפריט
+menu();//menu
 
-let navLinks = document.querySelector(".links");
-let links = document.querySelectorAll(".links li");
+popUpInfoInUpPage();//pop up info icon
 
-let hamburger = document.querySelector(".hamburger").addEventListener(
-    "click",
-    function () {
+helpChat();//help chat
 
-        navLinks.classList.toggle("open"); //פותח את התפריא במסך טלפון
+addToShopping();//add product
 
-        for (let i = 0; i < links.length; i++) {
-            links[i].classList.toggle("fade") //לפי  מציג את הנתון
+
+
+
+
+
+function menu() {
+
+    let navLinks = document.querySelector(".links");
+    let links = document.querySelectorAll(".links li");
+
+    let hamburger = document.querySelector(".hamburger").addEventListener(
+        "click",
+        function () {
+
+            navLinks.classList.toggle("open"); //פותח את התפריא במסך טלפון
+
+            for (let i = 0; i < links.length; i++) {
+                links[i].classList.toggle("fade") //לפי  מציג את הנתון
+            }
+        })
+}
+
+
+
+
+function popUpInfoInUpPage() {
+
+    let modal = document.querySelector('#my-modalPopUp');
+    let modalBtn = document.querySelector('#modalButtom-btn');
+    let closeBtn = document.querySelector('.closePop');
+
+    modalBtn.addEventListener( //לחיצה
+        'click',
+        function () {
+            modal.style.display = 'block';
         }
-    })
+    );
 
 
+    closeBtn.addEventListener( //לסגור אם איקס
+        'click',
+        function () {
+            modal.style.display = 'none';
+        }
+    );
 
 
+    window.addEventListener( //לסגור ברקע
+        'click', outsideClick
+    );
 
-
-
-
-///////////////////// לחיצה על הכפתור בתמונה ולהציג חלון עם הודעה
-
-
-let modal = document.querySelector('#my-modalPopUp');
-let modalBtn = document.querySelector('#modalButtom-btn');
-let closeBtn = document.querySelector('.closePop');
-
-modalBtn.addEventListener( //לחיצה
-    'click',
-    function () {
-        modal.style.display = 'block';
+    function outsideClick(e) {
+        if (e.target == modal) {
+            modal.style.display = 'none';
+        }
     }
-);
 
-
-closeBtn.addEventListener( //לסגור אם איקס
-    'click',
-    function () {
-        modal.style.display = 'none';
-    }
-);
-
-
-window.addEventListener( //לסגור ברקע
-    'click', outsideClick
-);
-
-function outsideClick(e) {
-    if (e.target == modal) {
-        modal.style.display = 'none';
-    }
 }
 
 
@@ -106,191 +117,110 @@ let closeFormB = function () {
 
 
 
+function helpChat() {
 
 
+    ///////////////////////////////////////חלונית עזרה ללקוחות
+    let toggleChatboxBtn = document.querySelector(".js-chatbox-toggle");
+    let chatbox = document.querySelector(".js-chatbox");
+    let chatboxMsgDisplay = document.querySelector(".js-chatbox-display");
+    let chatboxForm = document.querySelector(".js-chatbox-form");
 
 
-///////////////////////////////////////חלונית עזרה ללקוחות
-let toggleChatboxBtn = document.querySelector(".js-chatbox-toggle");
-let chatbox = document.querySelector(".js-chatbox");
-let chatboxMsgDisplay = document.querySelector(".js-chatbox-display");
-let chatboxForm = document.querySelector(".js-chatbox-form");
-
-
-toggleChatboxBtn.addEventListener("click", () => { //לחיצה כדאי שיופיע החלונית
-    chatbox.classList.toggle("chatbox--is-visible");
-});
-
-
-
-const createChatBubble = input => {
-    const chatSection = document.createElement("p"); //יצירת כתב מסוג 
-    chatSection.textContent = input; //להציג את התגובה בחלון של שירות שרשמנו
-    chatSection.classList.add("chatbox__display-chat");
-    chatboxMsgDisplay.appendChild(chatSection);
-};
-
-
-
-//לחיצה על כפתור שליחה הודעה בצאט
-chatboxForm.addEventListener("submit", e => {
-    let chatInput = document.querySelector(".js-chatbox-input").value;
-
-    createChatBubble(chatInput);
-
-    e.preventDefault();
-    chatboxForm.reset();
-});
-
-
-
-
-
-
-
-
-
-
-
-//מועדפים
-
-
-// let iconShoppingZ = document.querySelector('.iconShopping159');
-// let cartCloseBtnZ = document.querySelector('.closeZ');
-// let cartBoxZ = document.querySelector('.cartBoxXX');
-// iconShoppingZ.addEventListener("click", function () {
-//     cartBoxZ.classList.add('active');
-// });
-// cartCloseBtnZ.addEventListener("click", function () {
-//     cartBoxZ.classList.remove('active');
-// });
-
-
-// let attToCartBtnZ = document.getElementsByClassName('attToCart12');
-// let itemsZ = [];
-// for (let i = 0; i < attToCartBtnZ.length; i++) {
-//     attToCartBtnZ[i].addEventListener("click", function (e) {
-//         if (typeof (Storage) !== 'undefined') {
-//             let itemZ = {
-//                 idZ: i + 700,
-//                 nameZ: e.target.parentElement.children[0].textContent,
-//                 priceZ: e.target.parentElement.children[1].children[0].textContent,
-//                 noZ: 1
-//             };
-//             if (JSON.parse(localStorage.getItem('itemsZ')) === null) {
-//                 itemsZ.push(itemZ);
-//                 localStorage.setItem("itemsZ", JSON.stringify(itemsZ));
-//                 window.location.reload();
-//             } else {
-//                 let localItemsZ = JSON.parse(localStorage.getItem("itemsZ"));
-//                 localItemsZ.map(data => {
-//                     if (itemZ.idZ == data.idZ) {
-//                         itemZ.noZ = data.noZ + 1;
-//                     } else {
-//                         itemsZ.push(data);
-//                     }
-//                 });
-//                 itemsZ.push(itemZ);
-//                 localStorage.setItem('itemsZ', JSON.stringify(itemsZ));
-//                 window.location.reload();
-//             }
-//         }
-//     });
-// }
-
-// let iconShoppingPZ = document.querySelector('.iconShopping159 p');
-// let no1Z = 0;
-// JSON.parse(localStorage.getItem('itemsZ')).map(data => {
-//     no1Z += Number(data.noZ);
-// });
-// iconShoppingPZ.innerHTML = no1Z;
-
-
-// let cardBoxTableZ = cartBoxZ.querySelector('table');
-// let tableDataZ = '';
-
-
-// JSON.parse(localStorage.getItem('itemsZ')).map(data => {
-//     tableDataZ += '<tr class="BoxIt"><th>' + data.idZ + '</th><th class="vv">' + 'name: ' + data.nameZ + ' | ' + '</th><th class="ss">' +  '</th><th class="xx">' + 'price : ' + data.priceZ + ' | ' + '</tr>';
-// });
-
-// cardBoxTableZ.innerHTML = tableDataZ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//סל
-
-window.onload = function () {
-
-    let iconShopping = document.querySelector('.iconShopping');
-    let cartCloseBtn = document.querySelector('.fa-close');
-    let cartBox = document.querySelector('.cartBox');
-    iconShopping.addEventListener("click", function () {
-        cartBox.classList.add('active');
-    });
-    cartCloseBtn.addEventListener("click", function () {
-        cartBox.classList.remove('active');
+    toggleChatboxBtn.addEventListener("click", () => { //לחיצה כדאי שיופיע החלונית
+        chatbox.classList.toggle("chatbox--is-visible");
     });
 
 
-    let attToCartBtn = document.getElementsByClassName('attToCart');
-    let items = [];
-    for (let i = 0; i < attToCartBtn.length; i++) {
-        attToCartBtn[i].addEventListener("click", function (e) {
-            if (typeof (Storage) !== 'undefined') {
-                let item = {
-                    id: i + 700,
-                    name: e.target.parentElement.children[0].textContent,
-                    price: e.target.parentElement.children[1].children[0].textContent,
-                    no: 1
-                };
-                if (JSON.parse(localStorage.getItem('items')) === null) {
-                    items.push(item);
-                    localStorage.setItem("items", JSON.stringify(items));
-                    window.location.reload();
-                } else {
-                    let localItems = JSON.parse(localStorage.getItem("items"));
-                    localItems.map(data => {
-                        if (item.id == data.id) {
-                            item.no = data.no + 1;
-                        } else {
-                            items.push(data);
-                        }
-                    });
-                    items.push(item);
-                    localStorage.setItem('items', JSON.stringify(items));
-                    window.location.reload();
-                }
-            }
+
+    const createChatBubble = input => {
+        const chatSection = document.createElement("p"); //יצירת כתב מסוג 
+        chatSection.textContent = input; //להציג את התגובה בחלון של שירות שרשמנו
+        chatSection.classList.add("chatbox__display-chat");
+        chatboxMsgDisplay.appendChild(chatSection);
+    };
+
+
+
+    //לחיצה על כפתור שליחה הודעה בצאט
+    chatboxForm.addEventListener("submit", e => {
+        let chatInput = document.querySelector(".js-chatbox-input").value;
+
+        createChatBubble(chatInput);
+
+        e.preventDefault();
+        chatboxForm.reset();
+    });
+}
+
+
+
+
+
+function addToShopping() {
+
+
+    //סל
+
+    window.onload = function () {
+        let iconShopping = document.querySelector('.iconShopping');
+        let cartCloseBtn = document.querySelector('.fa-close');
+        let cartBox = document.querySelector('.cartBox');
+        iconShopping.addEventListener("click", function () {
+            cartBox.classList.add('active');
         });
+        cartCloseBtn.addEventListener("click", function () {
+            cartBox.classList.remove('active');
+        });
+
+
+        let attToCartBtn = document.getElementsByClassName('attToCart');
+        let items = [];
+        for (let i = 0; i < attToCartBtn.length; i++) {
+            attToCartBtn[i].addEventListener("click", function (e) {
+                if (typeof (Storage) !== 'undefined') {
+                    let item = {
+                        id: i + 600,
+                        name: e.target.parentElement.children[0].textContent,
+                        price: e.target.parentElement.children[1].children[0].textContent,
+                        no: 1
+                    };
+                    if (JSON.parse(localStorage.getItem('items')) === null) {
+                        items.push(item);
+                        localStorage.setItem("items", JSON.stringify(items));
+                        window.location.reload();
+                    } else {
+                        let localItems = JSON.parse(localStorage.getItem("items"));
+                        localItems.map(data => {
+                            if (item.id == data.id) {
+                                item.no = data.no + 1;
+                            } else {
+                                items.push(data);
+                            }
+                        });
+                        items.push(item);
+                        localStorage.setItem('items', JSON.stringify(items));
+                        window.location.reload();
+                    }
+                }
+            });
+        }
+
+        let iconShoppingP = document.querySelector('.iconShopping p');
+        let no1 = 0;
+        JSON.parse(localStorage.getItem('items')).map(data => {
+            no1 += Number(data.no);
+        });
+        iconShoppingP.innerHTML = no1;
+
+
+        let cardBoxTable = cartBox.querySelector('table');
+        let tableData = '';
+
+        JSON.parse(localStorage.getItem('items')).map(data => {
+            tableData += '<tr class="BoxIt"><th>' + data.id + '</th><th class="vv">' + 'name: ' + data.name + ' | ' + '</th><th class="ss">' + 'Amount: ' + data.no + ' | ' + '</th><th class="xx">' + 'price : ' + data.price + ' | ' + '</th><th><a href="#" onclick=Delete(this);>Delete</a></th></tr>';
+        });
+
+        cardBoxTable.innerHTML = tableData;
     }
-
-    let iconShoppingP = document.querySelector('.iconShopping p');
-    let no1 = 0;
-    JSON.parse(localStorage.getItem('items')).map(data => {
-        no1 += Number(data.no);
-    });
-    iconShoppingP.innerHTML = no1;
-
-
-    let cardBoxTable = cartBox.querySelector('table');
-    let tableData = '';
-
-
-    JSON.parse(localStorage.getItem('items')).map(data => {
-        tableData += '<tr class="BoxIt"><th>' + data.id + '</th><th class="vv">' + 'name: ' + data.name + ' | ' + '</th><th class="ss">' + 'Amount: ' + data.no + ' | ' + '</th><th class="xx">' + 'price : ' + data.price + ' | ' + '</th><th><a href="#" onclick=Delete(this);>Delete</a></th></tr>';
-    });
-
-    cardBoxTable.innerHTML = tableData;
 }
